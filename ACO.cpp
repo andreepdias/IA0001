adcbd2340699948373e2c5d5df023fe69fc13857
andreepdias <andre.dias@msn.com>
2018-09-05 09:44:52 +0000

    drop pick

--- ACO.cpp
+++ ACO.cpp
@@ -9,12 +9,110 @@ typedef vector<vector<int> > vvi;
 #define WINDOW_X 700
 #define WINDOW_Y 700
 
+int dx[] = {-1, 0, 0, 1};
+int dy[] = {0, -1, 1, 0};
+
 struct Ant{
-    int x, y;
+    int x, y, raio;
     bool carregando;
-    Ant(int a, int b){
-        x = a, y = b;
+    Ant(int a, int b, int r){
+        x = a, y = b, raio = r;
         carregando = false;
+    }
+
+    int countItens(vvi &grid){
+        int a, b, sg = grid.size();
+        int cont = 0;
+
+        for(int i = -raio; i <= raio; i++){
+            for(int j = -raio; j <= raio; j++){
+                a = x + i;
+                b = y + j;
+
+                if(a < 0)
+                    a = sg - 1;
+                else if(a >= sg)
+                    a = 0;
+
+                if(b < 0)
+                    b = sg - 1;
+                else if(b >= sg)
+                    b = 0;
+
+                if(grid[a][b] == 1){
+                    cont++;
+                }
+            }
+        }
+        return cont;
+    }
+
+    void move(vvi &grid_ant){
+        int sg = grid_ant.size();
+        int r = rand() % 4;
+
+        grid_ant[x][y]--;
+
+        x += dx[r];
+        y += dy[r];
+
+        if(x < 0)
+            x = sg -1;
+        else if(x >= sg)
+            x = 0;
+
+        if(y < 0)
+            y = sg - 1;
+        else if(y >= sg)
+            y = 0;
+
+        grid_ant[x][y]++;
+    }
+
+    void drop(vvi &grid){
+
+        if(grid[x][y] == 1)
+            return;
+
+        int num_casas, num_itens;
+        double prob_drop, prob_rand;
+
+        num_casas = (2 * raio + 1) * (2 * raio + 1) - 1;
+        num_itens = countItens(grid);
+
+        prob_drop = (num_itens * 1.0 / num_casas);
+        prob_drop *= prob_drop;
+        prob_drop -= 0.0025;
+
+        prob_rand = (double) rand() / RAND_MAX;
+
+        if(prob_rand <= prob_drop){
+            carregando = false;
+            grid[x][y] = 1;
+        }
+    }
+
+    void pick(vvi &grid){
+
+        if(grid[x][y] == 0)
+            return;
+
+        int num_casas, num_itens;
+        double prob_pick, prob_rand;
+
+        num_casas = (2 * raio + 1) * (2 * raio + 1) - 1;
+        num_itens = countItens(grid) - 1;
+
+        prob_pick =  1 - (num_itens * 1.0 / num_casas);
+        prob_pick *= prob_pick;
+        prob_pick += 0.0025;
+
+        prob_rand = (double) rand() / RAND_MAX;
+
+        if(prob_rand <= prob_pick){
+            carregando = true;
+            grid[x][y] = 0;
+        }
     }
 };
 
@@ -23,7 +121,7 @@ void draw(vvi &grid, vvi &grid_ant, sf::RenderWindow &window){
     int sx = grid.size();
     int sy = grid[0].size();
 
-    sf::RectangleShape item(sf::Vector2f(WINDOW_X / (float) sx, WINDOW_X / (float) sx));
+    sf::RectangleShape item(sf::Vector2f(WINDOW_X / (double) sx, WINDOW_X / (double) sx));
 
     for(int i = 0; i < sx; i++){
         for(int j = 0; j < sy; j++){
@@ -34,14 +132,14 @@ void draw(vvi &grid, vvi &grid_ant, sf::RenderWindow &window){
                 item.setFillColor(sf::Color(255,255,255));
             }
 
-            item.setPosition(WINDOW_X * i / (float)sx, WINDOW_Y * j / (float) sy);
+            item.setPosition(WINDOW_X * i / (double)sx, WINDOW_Y * j / (double) sy);
             window.draw(item);
         }
     }
     window.display();
 }
 
-void initGrid(vvi &grid, vvi &grid_ant, vector<Ant> &ants, int tam_grid, int num_formigas, int num_itens){
+void initGrid(vvi &grid, vvi &grid_ant, vector<Ant> &ants, int tam_grid, int num_formigas, int num_itens, int tam_raio){
 
     srand(time(NULL));
 
@@ -63,7 +161,7 @@ void initGrid(vvi &grid, vvi &grid_ant, vector<Ant> &ants, int tam_grid, int num
         int x = rand() % tam_grid;
         int y = rand() % tam_grid;
 
-        ants.push_back(Ant(x, y));
+        ants.push_back(Ant(x, y, tam_raio));
     }
 }
 
@@ -86,9 +184,22 @@ int main(){
     vvi grid_ant;
     vector<Ant> ants;
 
-    initGrid(grid, grid_ant, ants, tam_grid, num_formigas, num_itens);
+    initGrid(grid, grid_ant, ants, tam_grid, num_formigas, num_itens, tam_raio);
 
+    int sa = ants.size();
+
+    int k = 0;
     while(num_iteracoes--){
+
+        for(int i = 0; i < sa; i++){
+            ants[i].move(grid_ant);
+
+            if(ants[i].carregando){
+                ants[i].drop(grid);
+            }else{
+                ants[i].pick(grid);
+            }
+        }
 
         sf::Event event;
         while(window.pollEvent(event)){
@@ -96,7 +207,12 @@ int main(){
                 window.close();
         }
 
-        draw(grid, grid_ant, window);
+        k++;
+
+        if(k == 25000){
+            draw(grid, grid_ant, window);
+            k = 0;
+        }
 
     }
     window.close();
